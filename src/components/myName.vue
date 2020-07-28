e85cf55418f75a643a625cde2638801c14bde6b1
was-coder <wasleadwears4u2c@gmail.com>
2020-07-28 07:46:50 +0400

    DISPLAY NAME
    Display my name to the center of the page

---
+++ src/components/myName.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="centered">
+        <h1>Adelakun Wasiu</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props : name
+}
+</script>
+
+<style scoped>
+    .centered {
+        background:#c8d1d4;
+        display: flex;
+        justify-content:center;
+        align-items:center;
+        min-height: 100vh;
+    }
+</style>
